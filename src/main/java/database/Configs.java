7f46470cbda85357a5e0ba0ffe97fd0f2b55b69f
Kirill Timofeev <kirill.timofeev.a@gmail.com>
2020-04-26 14:43:23 +0300

    You can still do non-static window size and localization Almost clean

---
+++ src/main/java/database/Configs.java
@@ -0,0 +1,9 @@
+package database;
+
+public class Configs {
+    protected String dbHost = "localhost";
+    protected String dbPort = "3306";
+    protected String dbUser = "root";
+    protected String dbPass = "zZzoOo20022002";
+    protected String dbName = "PolyQuest";
+}
--- src/sample/Database/Configs.java
+++
@@ -1,9 +0,0 @@
-package sample.Database;
-
-public class Configs {
-    protected String dbHost = "localhost";
-    protected String dbPort = "3306";
-    protected String dbUser = "root";
-    protected String dbPass = "zZzoOo20022002";
-    protected String dbName = "polyquest";
-}
